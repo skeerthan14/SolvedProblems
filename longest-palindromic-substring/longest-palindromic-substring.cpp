dba0f2bdddcb6e472be4248dae953fc1737c6c7c
skeerthan14 <81695372+skeerthan14@users.noreply.github.com>
2021-09-02 10:50:44 +0530

    Time: 26 ms (75.15%), Space: 14.2 MB (49.46%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool isPalin(string s, int l, int h) {
-        if(l<0 || h>=s.size()) return false;
-        while(l<h) {
-            if(s[l++]!=s[h--]) return false;
-        }
-        return true;
-    }
+    // bool isPalin(string s, int l, int h) {
+    //     if(l<0 || h>=s.size()) return false;
+    //     while(l<h) {
+    //         if(s[l++]!=s[h--]) return false;
+    //     }
+    //     return true;
+    // }
     string longestPalindrome(string s) {
         string ans = "";
         int p1, p2;
@@ -21,16 +21,16 @@ public:
                 ans = s.substr(p1+1, p2-p1-1);
             }
             p1 = i, p2 = i+1;
-            cout<<p1<<" "<<p2<<" : ";
+            // cout<<p1<<" "<<p2<<" : ";
             while(p1>=0 && p2<n && s[p1]==s[p2]){
                 // cout<<p1<<" "<<p2<<" "<<(s[p1]==s[p2])<<" ;";
                 p1--;
                 p2++;
             }
-            cout<<" : "<<p1<<" "<<p2<<endl;
+            // cout<<" : "<<p1<<" "<<p2<<endl;
             if(p2-p1-1>ans.size()) {
                 ans = s.substr(p1+1, p2-p1-1);
-                cout<<ans<<endl;
+                // cout<<ans<<endl;
                 
             }
         }
