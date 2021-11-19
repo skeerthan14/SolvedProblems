866f0078ccd313a977338e3143bd92f3f33eb398
skeerthan14 <81695372+skeerthan14@users.noreply.github.com>
2021-11-19 20:13:17 +0530

    Time: 4 ms (70.57%), Space: 7.7 MB (56.96%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    void solve(int idx, vector<vector<int>>&ans, vector<int>&nums) {
-        if(idx==nums.size()) {
-            ans.push_back(nums);
+    void generate(vector<int> &nums, vector<int>&v, vector<vector<int>>&ans, bitset<10>&b) {
+        if(nums.size()==v.size()) {
+            ans.push_back(v);
             return;
         }
-        for(int i=idx;i<nums.size();i++) {
-            swap(nums[i], nums[idx]);
-            solve(idx+1, ans, nums);
-            swap(nums[i], nums[idx]);
-            
+
+        for(int i=0;i<nums.size();i++) {
+            if(b[i]==false){
+                v.push_back(nums[i]);
+                b[i] = true;
+                generate(nums, v, ans, b);
+                v.pop_back();
+                b[i] = false;
+            }
+
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        solve(0, ans, nums);
+        vector<int> v;
+        bitset<10>b;
+        generate(nums, v, ans, b);
         return ans;
     }
 };
