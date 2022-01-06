3cb1ece42c18ce15df59c5128ad1310f98e47513
skeerthan14 <81695372+skeerthan14@users.noreply.github.com>
2022-01-06 16:41:32 +0530

    Time: 12 ms (76.75%), Space: 10.2 MB (60.37%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<pair<int, int>> v;
+        for(int i=0;i<trips.size();i++) {
+            v.push_back({trips[i][1], trips[i][0]});
+            v.push_back({trips[i][2], -trips[i][0]});
+        }
+        int curr = 0;
+        sort(v.begin(), v.end());
+        for(int i=0;i<v.size();i++) {
+            curr += v[i].second;
+            if(curr>capacity) return false;
+        }
+        return true;
+    }
+};
